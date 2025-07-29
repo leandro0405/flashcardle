1d14adcb95816da28ad0e1c3a2120794894718cd
leandro0405 <162132097+leandro0405@users.noreply.github.com>
2025-07-29 09:14:34 -0300

    Add files via upload

---
+++ flashcardle-main/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é Javascript',
+    'O javascript é uma linguagem de progamação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Paraná',
+    'A capital do Paraná é Curitiva'
+)
+
+criaCartao(
+    'Progamação',
+    'O que é python?',
+    'O Python é uma linguagem de progamação'
+)
